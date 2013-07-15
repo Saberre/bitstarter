46b286b825d25d90409f6546430b9d23c479609c
Ki-baek Lee <saberre@gmail.com>
2013-07-15 01:39:09 +0000

    new grader.js

--- grader.js
+++ grader.js
@@ -15,6 +15,9 @@ References:
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
+ + restler
+   - https://github.com/danwrong/Restler/
+
  + JSON
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
@@ -24,6 +27,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = 'index.html';
 var CHECKSFILE_DEFAULT = 'checks.json';
 
@@ -44,15 +48,19 @@ var loadChecks = function (checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function (htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function ($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for (var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    return out;    
+};
+
+var checkHtmlFile = function (htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    return checkHtml($);
 };
 
 var clone = function (fn) {
@@ -65,10 +73,26 @@ if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+        rest.get(program.url).on('success', function (data) {
+            $ = cheerio.load(data);
+            var checkJson = checkHtml($, program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        }).on('error', function (data) {
+            console.log('An error occurred while fetching URL %s. Exiting.', program.url);
+            process.exit(1);
+        }).on('fail', function (data, response) {
+            console.log('Fetch failed with response code %d. Exiting.', response.statusCode);
+            process.exit(1);
+        });
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
